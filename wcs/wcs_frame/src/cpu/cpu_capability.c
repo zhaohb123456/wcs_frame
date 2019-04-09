df314394bce7b427ce45852f6e139f052d686b09
zhaohb123456 <zhaohengbin2012@sina.com>
2019-04-09 17:12:18 +0800

    sync code

--- wcs/wcs_frame/src/cpu/cpu_capability.c
+++ wcs/wcs_frame/src/cpu/cpu_capability.c
@@ -1,33 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cpu_capability.h"
-#include "../system_compatible.h"
-
-#if defined(WINDOWS)
-#include <windows.h>
-
-#elif defined(LINUX)
 #include <sys/sysinfo.h>
 
-#endif // 
 
 
 int CpuGetCoreNum(void)
 {
-#if defined(WINDOWS)
-	SYSTEM_INFO info;
-	memset(&info,0,sizeof(info));
-	GetSystemInfo(&info);
-	printf("PROCmask= %d\n",info.dwActiveProcessorMask);
-	printf("PROCLEVEL= %d\n", info.wProcessorLevel);
 
-	return info.dwNumberOfProcessors;
-#elif defined(LINUX)
 	return get_nprocs();
-
-#else
-	return UNSUPPORTED_SYSTEM;
-#endif // 
-
 
 }
